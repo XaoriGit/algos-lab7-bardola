14d20910bd7808461e6877c7092510b38cd404ac
XaoriGit <xaoriNit@gmail.com>
2024-12-07 10:14:59 +0600

    my work

--- main.py
+++ main.py
@@ -31,3 +31,41 @@
 
 Пример использования isPalindromString("Madam, Anna went to the civic center") # ["madam", "anna", "civic"]
 """
+import re
+
+
+def isPalindrom(str):
+    str = re.sub(r'[^A-zА-я]', '', str).lower()
+    if str == ''.join(reversed(str)):
+        return True
+    else:
+        return False
+
+print(isPalindrom("levE l"))
+print(isPalindrom("hel lo"))
+
+def isPalindromList(list):
+    res = []
+    for i in list:
+        str = re.sub(r'[^A-zА-я]', '', i).lower()
+        if str == ''.join(reversed(str)):
+            res.append(str)
+        else:
+            pass
+
+    return res
+
+print(isPalindromList(["hello", "list", "level"]))
+
+def isPalindromString(str):
+    mylist = re.sub(r'[^A-zА-я ]', '', str).lower().split(' ')
+    res = []
+    for i in mylist:
+        if i == ''.join(reversed(i)):
+            res.append(i)
+        else:
+            pass
+
+    return res
+
+print(isPalindromString("Madam, Anna went to the civic center"))
